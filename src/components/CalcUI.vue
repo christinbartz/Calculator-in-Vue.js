c57e552bdd63eeae04e7f8d249a3de22f1a9dfbe
Christin Bartz <cb.christinbartz@gmail.com>
2018-10-13 21:32:07 +0200

    setup structure
    scss in external file, add method and data object, add click handler

--- src/components/CalcUI.vue
+++ src/components/CalcUI.vue
@@ -2,32 +2,32 @@
   <div class="calc">
     <div class="calc__row calc__display">1235456</div>
     <div class="calc__row">
-      <div id="" class="calc__btn calc__btn--">C</div>
-      <div id="" class="calc__btn calc__btn--">+/-</div>
-      <div id="" class="calc__btn calc__btn--">%</div>
-      <div id="" class="calc__btn calc__btn--">/</div>
+      <div class="calc__btn calc__btn--">C</div>
+      <div class="calc__btn calc__btn--">+/-</div>
+      <div class="calc__btn calc__btn--">%</div>
+      <div class="calc__btn calc__btn--">/</div>
     </div>
     <div class="calc__row">
-      <div id="" class="calc__btn calc__btn--num">7</div>
-      <div id="" class="calc__btn calc__btn--num">8</div>
-      <div id="" class="calc__btn calc__btn--num">9</div>
-      <div id="" class="calc__btn calc__btn--operator">x</div>
+      <div @click="clickHandler('7')" class="calc__btn calc__btn--num">7</div>
+      <div @click="clickHandler('8')" class="calc__btn calc__btn--num">8</div>
+      <div @click="clickHandler('9')" class="calc__btn calc__btn--num">9</div>
+      <div @click="clickHandler('*')" class="calc__btn calc__btn--operator">x</div>
     </div>
     <div class="calc__row">
-      <div id="" class="calc__btn calc__btn--num">4</div>
-      <div id="" class="calc__btn calc__btn--num">5</div>
-      <div id="" class="calc__btn calc__btn--num">6</div>
-      <div id="" class="calc__btn calc__btn--operator">-</div>
+      <div @click="clickHandler('4')" class="calc__btn calc__btn--num">4</div>
+      <div @click="clickHandler('5')" class="calc__btn calc__btn--num">5</div>
+      <div @click="clickHandler('6')" class="calc__btn calc__btn--num">6</div>
+      <div @click="clickHandler('-')" class="calc__btn calc__btn--operator">-</div>
     </div>
     <div class="calc__row">
-      <div id="" class="calc__btn calc__btn--num">1</div>
-      <div id="" class="calc__btn calc__btn--num">2</div>
-      <div id="" class="calc__btn calc__btn--num">3</div>
-      <div id="" class="calc__btn calc__btn--operator">+</div>
+      <div @click="clickHandler('1')" class="calc__btn calc__btn--num">1</div>
+      <div @click="clickHandler('2')" class="calc__btn calc__btn--num">2</div>
+      <div @click="clickHandler('3')" class="calc__btn calc__btn--num">3</div>
+      <div @click="clickHandler('+')" class="calc__btn calc__btn--operator">+</div>
     </div>
     <div class="calc__row">
-      <div id="" class="calc__btn calc__btn--num">0</div>
-      <div id="" class="calc__btn calc__btn--num">.</div>
+      <div @click="clickHandler('0')" class="calc__btn calc__btn--num">0</div>
+      <div @click="clickHandler('.')" class="calc__btn calc__btn--num">.</div>
       <div id="" class="calc__btn calc__btn--operator">=</div>
     </div>
   </div>
@@ -36,18 +36,19 @@
 <script>
 
 export default {
-  name: 'CalcUI'
+  name: 'CalcUI',
+  data: () => {
+    evalArray: []
+  },
+  methods: {
+    clickHandler: (msg) => {
+      console.log(msg)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.calc {
-  display: flex;
-  width: 464px;
-  height: 640px;
-  &__row {
-    flex-direction: row;
-  }
-}
+  @import '../styles/CalcUI.scss';
 </style>
 
