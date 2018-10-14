0d526574d1f0f05eb4fb8c9b7b556486fcc5360d
Christin Bartz <cb.christinbartz@gmail.com>
2018-10-14 21:58:19 +0200

    calculator styling
    button and screen size, colors and scss variables

--- src/components/CalcUI.vue
+++ src/components/CalcUI.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="calc">
-    <div class="calc__row calc__display">1235456</div>
-    <div class="calc__row">
-      <div class="calc__btn calc__btn--">C</div>
-      <div class="calc__btn calc__btn--">+/-</div>
-      <div class="calc__btn calc__btn--">%</div>
-      <div class="calc__btn calc__btn--">/</div>
+    <div class="calc__display">
+     <div> 1235456 </div>
     </div>
-    <div class="calc__row">
-      <div @click="clickHandler('7')" class="calc__btn calc__btn--num">7</div>
-      <div @click="clickHandler('8')" class="calc__btn calc__btn--num">8</div>
-      <div @click="clickHandler('9')" class="calc__btn calc__btn--num">9</div>
-      <div @click="clickHandler('*')" class="calc__btn calc__btn--operator">x</div>
-    </div>
-    <div class="calc__row">
-      <div @click="clickHandler('4')" class="calc__btn calc__btn--num">4</div>
-      <div @click="clickHandler('5')" class="calc__btn calc__btn--num">5</div>
-      <div @click="clickHandler('6')" class="calc__btn calc__btn--num">6</div>
-      <div @click="clickHandler('-')" class="calc__btn calc__btn--operator">-</div>
-    </div>
-    <div class="calc__row">
-      <div @click="clickHandler('1')" class="calc__btn calc__btn--num">1</div>
-      <div @click="clickHandler('2')" class="calc__btn calc__btn--num">2</div>
-      <div @click="clickHandler('3')" class="calc__btn calc__btn--num">3</div>
-      <div @click="clickHandler('+')" class="calc__btn calc__btn--operator">+</div>
-    </div>
-    <div class="calc__row">
-      <div @click="clickHandler('0')" class="calc__btn calc__btn--num">0</div>
-      <div @click="clickHandler('.')" class="calc__btn calc__btn--num">.</div>
-      <div id="" class="calc__btn calc__btn--operator">=</div>
+    <div class="calc__btn-container">
+      <div class="calc__row">
+        <div class="calc__btn calc__btn--">C</div>
+        <div class="calc__btn calc__btn--">+/-</div>
+        <div class="calc__btn calc__btn--">%</div>
+        <div class="calc__btn calc__btn--">/</div>
+      </div>
+      <div class="calc__row">
+        <div @click="clickHandler('7')" class="calc__btn calc__btn--num">7</div>
+        <div @click="clickHandler('8')" class="calc__btn calc__btn--num">8</div>
+        <div @click="clickHandler('9')" class="calc__btn calc__btn--num">9</div>
+        <div @click="clickHandler('*')" class="calc__btn calc__btn--operator">x</div>
+      </div>
+      <div class="calc__row">
+        <div @click="clickHandler('4')" class="calc__btn calc__btn--num">4</div>
+        <div @click="clickHandler('5')" class="calc__btn calc__btn--num">5</div>
+        <div @click="clickHandler('6')" class="calc__btn calc__btn--num">6</div>
+        <div @click="clickHandler('-')" class="calc__btn calc__btn--operator">-</div>
+      </div>
+      <div class="calc__row">
+        <div @click="clickHandler('1')" class="calc__btn calc__btn--num">1</div>
+        <div @click="clickHandler('2')" class="calc__btn calc__btn--num">2</div>
+        <div @click="clickHandler('3')" class="calc__btn calc__btn--num">3</div>
+        <div @click="clickHandler('+')" class="calc__btn calc__btn--operator">+</div>
+      </div>
+      <div class="calc__row">
+        <div @click="clickHandler('0')" class="calc__btn calc__btn--num calc__btn--double">0</div>
+        <div @click="clickHandler('.')" class="calc__btn calc__btn--num">.</div>
+        <div id="" class="calc__btn calc__btn--operator">=</div>
+      </div>
     </div>
   </div>
 </template>
