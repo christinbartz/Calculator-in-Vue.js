84fed5d2cb761226c6089ce911655b3ab906a8d5
Christin Bartz <cb.christinbartz@gmail.com>
2018-10-24 21:38:09 +0200

    manage number and operator push
    seperate number and operator functions,
    manage adding of the minus operator

--- src/components/CalcUI.vue
+++ src/components/CalcUI.vue
@@ -7,32 +7,31 @@
     </div>
     <div class="calc__btn-container">
       <div class="calc__row">
-        <div @click="clearDisplay()" class="calc__btn">C</div>
+        <div @click="clearDisplay()" class="calc__btn calc__btn--double">C</div>
         <div @click="backspaceHandler()" class="calc__btn"><--</div>
-        <div class="calc__btn calc__btn--">+/-</div>
-        <div @click="clickHandler('/')" class="calc__btn calc__btn--operator">/</div>
+        <div @click="opHandler('/')" class="calc__btn calc__btn--operator">/</div>
       </div>
       <div class="calc__row">
-        <div @click="clickHandler('7')" class="calc__btn calc__btn--num">7</div>
-        <div @click="clickHandler('8')" class="calc__btn calc__btn--num">8</div>
-        <div @click="clickHandler('9')" class="calc__btn calc__btn--num">9</div>
-        <div @click="clickHandler('*')" class="calc__btn calc__btn--operator">x</div>
+        <div @click="numHandler('7')" class="calc__btn calc__btn--num">7</div>
+        <div @click="numHandler('8')" class="calc__btn calc__btn--num">8</div>
+        <div @click="numHandler('9')" class="calc__btn calc__btn--num">9</div>
+        <div @click="opHandler('*')" class="calc__btn calc__btn--operator">x</div>
       </div>
       <div class="calc__row">
-        <div @click="clickHandler('4')" class="calc__btn calc__btn--num">4</div>
-        <div @click="clickHandler('5')" class="calc__btn calc__btn--num">5</div>
-        <div @click="clickHandler('6')" class="calc__btn calc__btn--num">6</div>
-        <div @click="clickHandler('-')" class="calc__btn calc__btn--operator">-</div>
+        <div @click="numHandler('4')" class="calc__btn calc__btn--num">4</div>
+        <div @click="numHandler('5')" class="calc__btn calc__btn--num">5</div>
+        <div @click="numHandler('6')" class="calc__btn calc__btn--num">6</div>
+        <div @click="opHandler('-')" class="calc__btn calc__btn--operator">-</div>
       </div>
       <div class="calc__row">
-        <div @click="clickHandler('1')" class="calc__btn calc__btn--num">1</div>
-        <div @click="clickHandler('2')" class="calc__btn calc__btn--num">2</div>
-        <div @click="clickHandler('3')" class="calc__btn calc__btn--num">3</div>
-        <div @click="clickHandler('+')" class="calc__btn calc__btn--operator">+</div>
+        <div @click="numHandler('1')" class="calc__btn calc__btn--num">1</div>
+        <div @click="numHandler('2')" class="calc__btn calc__btn--num">2</div>
+        <div @click="numHandler('3')" class="calc__btn calc__btn--num">3</div>
+        <div @click="opHandler('+')" class="calc__btn calc__btn--operator">+</div>
       </div>
       <div class="calc__row">
-        <div id="btn-zero" @click="clickHandler('0')" class="calc__btn calc__btn--num calc__btn--double">0</div>
-        <div @click="clickHandler('.')" class="calc__btn calc__btn--num">.</div>
+        <div id="btn-zero" @click="numHandler('0')" class="calc__btn calc__btn--num calc__btn--double">0</div>
+        <div @click="opHandler('.')" class="calc__btn calc__btn--num">.</div>
         <div @click="currentResult()" id="btn-equal" class="calc__btn calc__btn--operator">=</div>
       </div>
     </div>
@@ -49,17 +48,30 @@ export default {
     }
   },
   methods: {
-    clickHandler: function(val) {
+    opHandler: function(val) {
+      if(val === '-') {
+        if(this.evalArray[this.evalArray.length -1] === '-') {
+          return
+        } else {
+          this.evalArray.push(val)
+        }
+      } else {
+        if(this.evalArray[this.evalArray.length - 1] !== '*') {
+
+        }
+      }
+    },
+    numHandler: function(val) {
       this.evalArray.push(val)
     },
     clearDisplay: function() {
-      this.evalArray = []
+      this.evalArray = [0]
     },
     backspaceHandler: function() {
       this.evalArray.pop()
     },
     currentResult: function() {
-      console.log(eval(this.displayEvalArray))
+      this.evalArray = [eval(this.displayEvalArray)]
     }
   },
   computed: {
